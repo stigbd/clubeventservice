2615e151dd64c3eeb08f27def3a1fce7c21c7962
Stig B. Dørmænen <stigbd@gmail.com>
2017-11-29 21:30:47 +0100

    Load formats upon start

--- src/server.js
+++ src/server.js
@@ -7,7 +7,9 @@ let morgan = require('morgan')
 let mongoose = require('mongoose')
 let bodyParser = require('body-parser')
 let Competition = require('./models/competition')
+let Format = require('./models/format')
 require('dotenv').config()
+var path = require('path')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -37,6 +39,33 @@ mongoose.connect(uri, options)
     console.error('Error while trying to connect with mongodb')
     throw err
   })
+
+// ===== Load default data ====
+// Loading formats:
+const loadJsonFile = require('load-json-file')
+var file = path.join(__dirname, '/data/formats.json')
+loadJsonFile(file)
+  .then(formats => {
+    for (var format of formats) {
+      console.log(format)
+      var data = new Format(format)
+      // ---- save logic start
+      data
+        .save()
+        .then(saved => console.log('saved', saved))
+        .catch(err => {
+          if (err.code === 11000) {
+            return console.log('Object already saved')
+          }
+          console.error('err while saving', err)
+        })
+      // ---- save logic end
+    }
+  })
+  .catch(err => {
+    console.error(err)
+  })
+
 // ===== Public Routes =====
 
 // Get root
